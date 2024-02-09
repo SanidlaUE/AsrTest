1516165e7fbde1f950654e003e6a975b2e3413a9
Denis Shamarakov <denis.shamarakov@ravnur.com>
2024-02-09 14:01:44 +0200

    extensions

---
+++ AsrTest/Extensions/MainExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AsrTest.Extensions
+{
+    public static class MyConfigServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConfig(
+             this IServiceCollection services, IConfiguration config)
+        {
+            //services.Configure<PositionOptions>(
+            //    config.GetSection(PositionOptions.Position));
+            //services.Configure<ColorOptions>(
+            //    config.GetSection(ColorOptions.Color));
+
+            return services;
+        }
+
+        public static IServiceCollection AddMyDependencyGroup(
+             this IServiceCollection services)
+        {
+            //services.AddScoped<IMyDependency, MyDependency>();
+            //services.AddScoped<IMyDependency2, MyDependency2>();
+            
+            return services;
+        }
+    }
+}
